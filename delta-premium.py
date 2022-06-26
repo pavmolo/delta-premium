5fcbd70332f17e4acafe900edc8a0a3e6dd856af
pavmolo <74321783+pavmolo@users.noreply.github.com>
2022-06-26 19:42:05 +0300

    Update delta-premium.py

--- delta-premium.py
+++ delta-premium.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import os.path
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
@@ -84,14 +83,14 @@ def show_predict_page():
   st.markdown('''<a href="http://kaizen-consult.ru/"><img src='https://www.kaizen.com/images/kaizen_logo.png' style="width: 50%; margin-left: 25%; margin-right: 25%; text-align: center;"></a><p>''', unsafe_allow_html=True)
   st.title("Определи свой потенциал")
   val_list = ['Рубль', 'Доллар США']
-  val_0 = st.selectbox("Выберите валюту:", val_list, index=0)
+  val_0 = st.radio("Выберите валюту:", val_list, index=0)
   if val_0 == 'Рубль':
       val = 'млн₽'
   else:
       val = 'тыс$'
   st.subheader('Нам необходима информация, чтобы спрогнозировать ваши показатели прибыли')
-  industry = st.selectbox("Ваша отрасль:", industry_list)
-  market_state = st.selectbox("Охарактеризуйте состояние сектора, в котором вы работаете:", gro_state_list)
+  industry = st.radio("Ваша отрасль:", industry_list)
+  market_state = st.radio("Охарактеризуйте состояние сектора, в котором вы работаете:", gro_state_list)
   revenue = st.number_input(f"Какова ваша выручка, {val} в год:", value=0)
   margin = st.slider("Какова ваша маржа операционной прибыли, % к выручке:", -20, 80, 0, 2)
   growth = st.slider("Каков ваш среднегодовой рост выручки в % за последние 3 года", -20, 100, 0, 5)
