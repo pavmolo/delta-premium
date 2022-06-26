f12871e1b41743804b12e33f05766c52fbbc64ee
pavmolo <74321783+pavmolo@users.noreply.github.com>
2022-06-26 20:18:11 +0300

    Update delta-premium.py

--- delta-premium.py
+++ delta-premium.py
@@ -22,7 +22,6 @@ credentials = service_account.Credentials.from_service_account_info(st.secrets["
 SAMPLE_SPREADSHEET_ID = '1etUSlGdjQruAn5AjPCat2R-k9LhXnapnXm4szTZJ3Pg'
 service = build('sheets', 'v4', credentials=credentials).spreadsheets().values()
 
-@st.cache
 def df_maker(sheet, columns):
   resp = service.get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=sheet).execute()
   values = resp.get('values', [])
